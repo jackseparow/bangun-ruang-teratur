2ee036d7732406b0212b1b7de8ab567c2062df28
jackseparow <49458990+jackseparow@users.noreply.github.com>
2026-04-28 16:15:48 +0700

    Update blocks.js

--- blocks.js
+++ blocks.js
@@ -1,4 +1,3 @@
-// 1. Blok Bangun Dasar (Output: Shape)
 Blockly.Blocks['base_shape'] = {
   init: function() {
     this.appendDummyInput()
@@ -9,42 +8,35 @@ Blockly.Blocks['base_shape'] = {
   }
 };
 
-// 2. Blok Truncate (Nested + Input Kedalaman)
 Blockly.Blocks['truncate_shape'] = {
   init: function() {
-    this.appendValueInput("INPUT")
-        .setCheck("Shape")
-        .appendField("Pancung (Truncate)");
-    this.appendDummyInput()
-        .appendField("Kedalaman:")
-        .appendField(new Blockly.FieldNumber(0.3, 0, 0.5), "DEPTH");
+    this.appendValueInput("INPUT").setCheck("Shape").appendField("Pancung (Truncate)");
+    this.appendDummyInput().appendField("Kedalaman:").appendField(new Blockly.FieldNumber(0.3, 0, 0.5), "DEPTH");
     this.setOutput(true, "Shape");
     this.setColour(290);
   }
 };
 
-// 3. Blok Rectify (Nested)
 Blockly.Blocks['rectify_shape'] = {
   init: function() {
-    this.appendValueInput("INPUT")
-        .setCheck("Shape")
-        .appendField("Rectify");
+    this.appendValueInput("INPUT").setCheck("Shape").appendField("Rectification");
     this.setOutput(true, "Shape");
     this.setColour(290);
   }
 };
 
-// Generator JavaScript
+// Generator Logika
 Blockly.JavaScript = new Blockly.Generator('JavaScript');
 Blockly.JavaScript['base_shape'] = function(block) {
-  return [block.getFieldValue('SHAPE'), 0];
+  return ["'" + block.getFieldValue('SHAPE') + "'", 0];
 };
+
 Blockly.JavaScript['truncate_shape'] = function(block) {
-  var input = Blockly.JavaScript.valueToCode(block, 'INPUT', 0) || 'NONE';
-  var depth = block.getFieldValue('DEPTH');
-  return ["TRUNCATED_" + input + "_DEPTH_" + depth, 0];
+  var input = Blockly.JavaScript.valueToCode(block, 'INPUT', 0) || "'NONE'";
+  return ["'TRUNCATED_' + " + input, 0];
 };
+
 Blockly.JavaScript['rectify_shape'] = function(block) {
-  var input = Blockly.JavaScript.valueToCode(block, 'INPUT', 0) || 'NONE';
-  return ["RECTIFIED_" + input, 0];
+  var input = Blockly.JavaScript.valueToCode(block, 'INPUT', 0) || "'NONE'";
+  return ["'RECTIFIED_' + " + input, 0];
 };
