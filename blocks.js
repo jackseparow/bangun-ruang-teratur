31ca6d878970de7c3466d421cdbf8b3026f98708
jackseparow <49458990+jackseparow@users.noreply.github.com>
2026-04-28 16:25:00 +0700

    Update blocks.js

--- blocks.js
+++ blocks.js
@@ -9,7 +9,7 @@ Blockly.Blocks['base_shape'] = {
   }
 };
 
-// Blok Pancung (Truncate) - Mendukung Nested & Input Kedalaman
+// Blok Pancung (Truncate)
 Blockly.Blocks['truncate_shape'] = {
   init: function() {
     this.appendValueInput("INPUT").setCheck("Shape").appendField("Pancung (Truncate)");
@@ -28,19 +28,26 @@ Blockly.Blocks['rectify_shape'] = {
   }
 };
 
-// Generator JavaScript Khusus
+// Generator JavaScript menggunakan format JSON aman
 Blockly.JavaScript = new Blockly.Generator('JavaScript');
+Blockly.JavaScript.ORDER_ATOMIC = 0;
+
 Blockly.JavaScript['base_shape'] = function(block) {
   var code = block.getFieldValue('SHAPE');
-  return ["'" + code + "'", 0];
+  // Mengembalikan string tipe dasar
+  return ["\"" + code + "\"", Blockly.JavaScript.ORDER_ATOMIC];
 };
 
 Blockly.JavaScript['truncate_shape'] = function(block) {
-  var input = Blockly.JavaScript.valueToCode(block, 'INPUT', 0) || "'NONE'";
-  return ["'TRUNCATED_' + " + input, 0];
+  var input = Blockly.JavaScript.valueToCode(block, 'INPUT', Blockly.JavaScript.ORDER_ATOMIC) || "\"NONE\"";
+  // Menghapus tanda kutip ganda ekstra dari input nested
+  input = input.replace(/^\"|\"$/g, '');
+  return ["\"TRUNCATED_" + input + "\"", Blockly.JavaScript.ORDER_ATOMIC];
 };
 
 Blockly.JavaScript['rectify_shape'] = function(block) {
-  var input = Blockly.JavaScript.valueToCode(block, 'INPUT', 0) || "'NONE'";
-  return ["'RECTIFIED_' + " + input, 0];
+  var input = Blockly.JavaScript.valueToCode(block, 'INPUT', Blockly.JavaScript.ORDER_ATOMIC) || "\"NONE\"";
+  // Menghapus tanda kutip ganda ekstra dari input nested
+  input = input.replace(/^\"|\"$/g, '');
+  return ["\"RECTIFIED_" + input + "\"", Blockly.JavaScript.ORDER_ATOMIC];
 };
