b81e38d58da5acf2db98ae2596dff09b78483ed2
jackseparow <49458990+jackseparow@users.noreply.github.com>
2026-04-28 13:07:34 +0700

    Update blocks.js

--- blocks.js
+++ blocks.js
@@ -3,34 +3,48 @@ Blockly.Blocks['base_shape'] = {
   init: function() {
     this.appendDummyInput()
         .appendField("Bangun:")
-        .appendField(new Blockly.FieldDropdown([["Kubus","CUBE"], ["Tetrahedron","TETRA"], ["Oktahedron","OCTA"]]), "SHAPE");
+        .appendField(new Blockly.FieldDropdown([
+            ["Kubus","CUBE"], 
+            ["Tetrahedron","TETRA"], 
+            ["Oktahedron","OCTA"]
+        ]), "SHAPE");
     this.setOutput(true, "Shape");
     this.setColour(230);
   }
 };
 
-// Blok Teknik Pancung (Truncation)
+// Blok Truncate
 Blockly.Blocks['truncate_shape'] = {
   init: function() {
     this.appendValueInput("INPUT")
         .setCheck("Shape")
         .appendField("Pancung (Truncate)");
-    this.appendDummyInput()
-        .appendField("Kedalaman:")
-        .appendField(new Blockly.FieldNumber(0.3, 0, 0.5), "DEPTH");
     this.setOutput(true, "Shape");
     this.setColour(290);
   }
 };
 
-// Blok Teknik Rectification
+// Blok Rectify
 Blockly.Blocks['rectify_shape'] = {
   init: function() {
     this.appendValueInput("INPUT")
         .setCheck("Shape")
-        .appendField("Rectification");
-    this.setHelpUrl("https://en.wikipedia.org/wiki/Rectification_(geometry)");
+        .appendField("Rectify (Tengah Rusuk)");
     this.setOutput(true, "Shape");
     this.setColour(290);
   }
+};
+
+// Generator JavaScript Sederhana
+Blockly.JavaScript = new Blockly.Generator('JavaScript');
+Blockly.JavaScript['base_shape'] = function(block) {
+  return [block.getFieldValue('SHAPE'), Blockly.JavaScript.ORDER_ATOMIC];
+};
+Blockly.JavaScript['truncate_shape'] = function(block) {
+  var input = Blockly.JavaScript.valueToCode(block, 'INPUT', Blockly.JavaScript.ORDER_ATOMIC);
+  return ["TRUNCATED_" + input, Blockly.JavaScript.ORDER_ATOMIC];
+};
+Blockly.JavaScript['rectify_shape'] = function(block) {
+  var input = Blockly.JavaScript.valueToCode(block, 'INPUT', Blockly.JavaScript.ORDER_ATOMIC);
+  return ["RECTIFIED_" + input, Blockly.JavaScript.ORDER_ATOMIC];
 };
